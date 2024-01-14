3d74de17d4594da7bf7680ff6ae8e34d47f10ba4
David Nicholson <nickledave@users.noreply.github.com>
2024-01-14 15:07:52 -0500

    Add docs/ with conf.py, index.md, and makefiles

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'VocalPy'
+copyright = '2024, David Nicholson'
+author = 'David Nicholson'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx_copybutton',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx_design',
+    'sphinxext.opengraph',
+    'sphinx_tabs.tabs',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pdyata_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# The VocalPy Project
+
+The VocalPy project is a community effort to build a core Python package
+for researchers studying acoustic communicaiton
+and foster an ecosystem of interoperable packages.
